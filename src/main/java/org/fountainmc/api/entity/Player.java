e4cf5dd10f8fc5e755e5bc7a6f30c52efc134109
Techcable <Techcable@techcable.net>
2016-06-13 09:13:13 -0700

    Add Player.sendMessage(String) and Player.sendMessages(String...)
    Fixes #12

--- src/main/java/org/fountainmc/api/entity/Player.java
+++ src/main/java/org/fountainmc/api/entity/Player.java
@@ -1,12 +1,32 @@
 package org.fountainmc.api.entity;
 
 import java.util.UUID;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import com.google.common.base.Preconditions;
+
 import org.fountainmc.api.command.CommandSender;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+@ParametersAreNonnullByDefault
 public interface Player extends EntityLiving, CommandSender {
 
-    String getName();
-    
-    UUID getUUID();
-    
+    @Nonnull
+    public String getName();
+
+    @Nonnull
+    public UUID getUUID();
+
+    public void sendMessage(String message);
+
+    public default void sendMessages(String... messages) {
+        for (int i = 0; i < checkNotNull(messages, "Null messages array").length; i++) {
+            String message = messages[i];
+            checkNotNull(message, "Null message at index %s", i);
+            sendMessage(message);
+        }
+    }
 }
